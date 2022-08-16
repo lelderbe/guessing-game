42c259910eae519dea18d91b592aa079438f1954
lelderbe <fvdc@ya.ru>
2022-08-16 20:22:33 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.value = this.min + Math.ceil((this.max - this.min) / 2);
+        return this.value;
     }
 
     lower() {
-
+        this.max = this.value;
     }
 
     greater() {
-
+        this.min = this.value;
     }
 }
 
